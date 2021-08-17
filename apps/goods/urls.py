af0edc83bbea20cd149902a80e16b18a5288d791
简配 <root_pei@163.com>
2021-08-17 22:52:26 +0800

    添加注册功能

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf.urls import url
+from apps.goods import views
 
 urlpatterns = [
-
+    path('index', views.index, name='index')
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.urls import reverse
+from django.http import HttpResponse
+import re
+from apps.user.models import User
+
 
 # Create your views here.
+
+class RegisterView(View):
+    # /user/register
+    def get(self, request):
+        """获取注册页面"""
+        return render(request, 'register.html')
+
+    # /user/register
+    def post(self, request):
+        """注册处理"""
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+
+        # 进行数据校验
+        if not all([username, password, email]):
+            return render(request, 'register.html', {'error': '数据不完整'})
+
+        # 邮箱校验
+        if not re.match(r'[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+            return render(request, 'register.html', {'error': '邮箱格式不正确'})
+
+        # 业务处理进行注册
+        user = User.objects.create_user(username, password, email)
+
+        # 注册完跳转到首页
+        return redirect(reverse('goods:index'))
