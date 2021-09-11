eca0c13407b86e59b1f984c061174d478548e88d
简配 <root_pei@163.com>
2021-09-11 16:45:34 +0800

    完成添加购物车功能

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf.urls import url
+from .views import CartAddView
 
 urlpatterns = [
-
+    path('add/', CartAddView.as_view(), name='add')  # 购物车添加
 ]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import JsonResponse
+from apps.goods.models import GoodsSKU
+from django_redis import get_redis_connection
+from django.shortcuts import redirect, reverse
 
-# Create your views here.
+
+# 添加商品到购物车
+# 传递参数 商品id(sku_id) 商品数量(count)
+
+# /cart/add
+class CartAddView(View):
+    """购物车记录添加"""
+
+    def post(self, request):
+        """购物车记录添加"""
+        # 获取数据
+        user = request.user
+        print(user.id)
+        if not user.is_authenticated:
+            # 用户未登录
+            return JsonResponse({'res': 5, 'msg': '请先登录'})
+        sku_id = request.POST.get('sku_id')
+        count = request.POST.get('count')
+        # 数据校验
+        if not all([sku_id, count]):
+            return JsonResponse({'res': 0, 'msg': '数据不完整'})
+
+        # 校验添加的商品数量
+        try:
+            count = int(count)
+        except Exception as e:
+            return JsonResponse({'res': 1, 'msg': '数目出错'})
+
+        # 检验商品是否存在
+        try:
+            sku = GoodsSKU.objects.get(id=sku_id)
+        except GoodsSKU.DoesNotExist:
+            # 商品不存在
+            return JsonResponse({'res': 2, 'msg': '商品不存在'})
+
+        # 业务处理
+        conn = get_redis_connection('default')
+        cart_key = 'cart_%s' % user.id
+        # 获取sku_id的值 hget cart_key
+        # 如果sku_id在hash中不存在，则返回None
+        cart_count = conn.hget(cart_key, sku_id)
+        if cart_count:
+            # 如果用户hash有记录则追加count
+            count += int(cart_count)
+        if count > sku.stock:
+            return JsonResponse({'res': 4, 'msg': '商品库存不足'})
+        conn.hset(cart_key, sku_id, count)
+
+        total_count = conn.hlen(cart_key)
+        # 返回应答
+        return JsonResponse({'res': 3, 'total_count': total_count, 'msg': '添加成功'})
